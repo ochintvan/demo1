d97cb8fa1c05d84bf15221a6e6b1b0dfbc80ab8e
Chintvan <ochintvan@gmail.com>
2025-09-11 17:26:33 -0400

    Controller added

---
+++ src/main/java/com/example/demo1/controllers/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo1.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello()
+    {
+        return "Jai Swaminarayan";
+    }
+}
